9d0204c54708f55115bbf4c64882435c9973b20b
Charlie DeTar <cfd@media.mit.edu>
2011-07-21 12:45:25 -0600

    Fixing issue #4.  Thanks, @cburgmer

--- registration_defaults/settings.py
+++ registration_defaults/settings.py
@@ -1,4 +1,1 @@
-import os
-
 ACCOUNT_ACTIVATION_DAYS = 2
-REGISTRATION_TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), "templates")
--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 from distutils.core import setup
 from distutils.command.install import INSTALL_SCHEMES
+
 import os
 
-root = os.path.dirname(os.path.abspath(__file__))
-os.chdir(root)
-
-master_file = open(os.path.join(root, ".git", "refs", "heads", "master"))
-VERSION = '0.2.git-' + master_file.read().strip()
-master_file.close()
+VERSION = '0.3'
 
 # Make data go to the right place.
 # http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
